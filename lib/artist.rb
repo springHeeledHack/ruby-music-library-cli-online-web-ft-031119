42afdd82d1ed800179317901d3f2b46097fd03d0
kyle smith <kylemclellan@gmail.com>
2019-03-29 21:13:53 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++
@@ -1,9 +0,0 @@
-  def add_song(song)
-    song.artist = self unless song.artist
-    songs << song unless songs.include?(song)
-  end
-
-  def genres
-    songs.collect{ |s| s.genre }.uniq
-  end
-end
