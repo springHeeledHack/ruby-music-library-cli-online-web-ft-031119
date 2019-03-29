98ee3165fca0375fb449892a6bb2756b153e3783
kyle smith <kylemclellan@gmail.com>
2019-03-29 21:12:02 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,9 +1,3 @@
-ef self.create(name)
-    artist = new(name)
-    artist.save
-    artist
-  end
-
   def add_song(song)
     song.artist = self unless song.artist
     songs << song unless songs.include?(song)
